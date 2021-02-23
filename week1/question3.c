018be656e5eaab6c572b8529a92a5bc11fc328c5
Hasan Ali ÖZKAN <63359311+hasanaliozkan-dev@users.noreply.github.com>
2021-02-23 13:05:10 +0300

    Update question3.c

--- week1/question3.c
+++ week1/question3.c
@@ -5,18 +5,6 @@ int main(){
 double radius,area,circumf;
 int num_circ;
 char circ_name;
-int input = 0;
-while (input!=1){
-printf("\nPlease enter the name of your circle with just one char : ");
-scanf("%c",&circ_name);
-printf("\nPlease enter radius of your circle : ");
-scanf("%lf",&radius);
-area = PI*radius*radius;
-circumf = 2.0*PI*radius;
-printf("\nThe area of the \"%c\" circle is \"%lf\"\n",circ_name,area);
-printf("\nThe circumstance of the \"%c\" circle is \"%lf\"\n",circ_name,circumf);
-printf("\nIf you want to exit please press \"1\" otherwise press any key to continue : ");
-scanf("%d",&input);
 }
 
 
